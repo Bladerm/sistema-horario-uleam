689875476d0be9b52529ef80e2cb6a3be4de4e28
Bladerm <josemanuelcastilloloor@gmail.com>
2026-01-18 16:24:56 -0500

    arreglo 2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -18,8 +18,8 @@
       <form @submit.prevent="handleLogin">
         <div class="form-group">
           <label for="email">Ingrese su correo:</label>
-          <input 
-            <!-- Input de email con v-model para enlace bidireccional con el estado 'email' -->
+          <!-- Input de email con v-model para enlace bidireccional con el estado 'email' -->
+          <input
             type="email" 
             id="email"
             v-model="email"
